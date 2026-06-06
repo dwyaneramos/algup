4dfb055f189b34a66294b12d1eee806505b2dcd6
dwyaneramos <riz3ndrr@gmail.com>
2026-06-06 14:25:48 +1200

    feat: random AUF on both the start and end

--- server/src/routes/scramble.ts
+++ server/src/routes/scramble.ts
@@ -27,13 +27,21 @@ function generateMoveList(n: number): string {
   return moves.join(' ');
 }
 
+function generateAUF(): string {
+  const possibleAUFs = ['', 'U', 'U2', "U'"];
+  const choice = possibleAUFs[Math.floor(Math.random() * possibleAUFs.length)];
+  return choice;
+}
+
 router.post('/', async (req: Request, res: Response) => {
-  const { alg } = req.body;
+  let { alg } = req.body;
 
   if (!alg) {
     res.status(400).json({ error: 'alg is required' });
     return;
   }
+
+  alg = `${generateAUF()} ${alg} ${generateAUF()}`;
 
   try {
     const kpuzzle = await cube3x3x3.kpuzzle();
@@ -50,10 +58,10 @@ router.post('/', async (req: Request, res: Response) => {
       prefix = generateMoveList(prefixLength);
       parsed = new Alg(`${prefix} ${alg}`);
       pattern = kpuzzle.algToTransformation(parsed).toKPattern();
-      solution = await experimentalSolve3x3x3IgnoringCenters(pattern); scramble = new Alg(`${solution.toString()} ${prefix}`).experimentalSimplify({ cancel: true, puzzleLoader: cube3x3x3 }).toString();
+      solution = await experimentalSolve3x3x3IgnoringCenters(pattern);
+      scramble = new Alg(`${solution.toString()} ${prefix}`).experimentalSimplify({ cancel: true, puzzleLoader: cube3x3x3 }).toString();
     } while (scramble.split(" ").length < minimumScrambleLength);
 
-    console.log('prefix: ', prefix)
     res.status(200).json({ scramble: scramble });
     console.log(`Generated scramble for alg "${alg}" with length ${scramble.split(" ").length}: ${scramble}`);
   } catch (err) {
